b3a930389584f6aced8504279018b2a40024c680
LAD SHRUTI JAYANT <123343565+ladshruti@users.noreply.github.com>
2023-01-27 16:09:24 +0530

    Add files via upload
    Tourism Management System consist details about tourist details, ticket records. The passenger can check the favourite tourist place in the application and can book, cancel, update, search the bookings accordingly.

---
+++ Tourism Management System/src/jTable2.java
@@ -0,0 +1,19 @@
+
+import javax.swing.table.DefaultTableModel;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ladsh
+ */
+class jTable2 {
+
+    static void setModel(DefaultTableModel model) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
